9ded3d9cbc5ebf43653a2c15336e9d3b3144daf5
omasette <38251408+omasette@users.noreply.github.com>
2018-07-08 15:23:54 +0300

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,69 +1,79 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Property</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+<body>
 
-@section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form class="form-horizontal" action="/login" method="post"  encytype = "multipart/form-data">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }} name="emailAddress" value="{{$login->Name or ''}} </label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }} name="password" value="{{$login->Name or ''}}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                              @if ($errors->has('password'))
-                                   <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            <div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
+  <div class="topnav">
+    <a class="active" href="/">Home</a>
+    <a href="/property_table">property Table</a>
+  </div>
+  <h2>My Properties</h2>
+  <form class="form-horizontal" action="/property" method="post"  encytype = "multipart/form-data">
+{{csrf_field()}}
+    <div class="form-group">
+      <label class="control-label col-sm-2">Property Name:</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" placeholder="Enter Property Name" name="propertyName" value="{{$property->Name or ''}}">
+      </div>
     </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2">location:</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" placeholder="Enter property's location" name="locationName" value="{{$property->location or ''}}">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2">Description:</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" placeholder="Enter property'description" name="description" value="{{$property->description or ''}}" >
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2">contact:</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" placeholder="Enter landlord's contact" name="contacts" value="{{$property->contacts or ''}}">
+      </div>
+    </div>
+    <input type="text" name="id" value="{{$property->id or ''}}" readonly>
+    <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-10">
+        <button type="submit" class="btn btn-default">Submit</button>
+      </div>
+    </div>
+  </form>
 </div>
-@endsection
+<style>
+/* Add a black background color to the top navigation */
+.topnav {
+    background-color: #333;
+    overflow: hidden;
+}
+
+/* Style the links inside the navigation bar */
+.topnav a {
+    float: left;
+    color: #f2f2f2;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    font-size: 17px;
+}
+
+/* Change the color of links on hover */
+.topnav a:hover {
+    background-color: #ddd;
+    color: black;
+}
+
+/* Add a color to the active/current link */
+.topnav a.active {
+    background-color: #4CAF50;
+    color: white;
+}
+</style>
+</body>
+</html>
