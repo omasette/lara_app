7522cacaafbeac84effdf42270d93b1279f4ac0d
omasette <38251408+omasette@users.noreply.github.com>
2018-07-08 19:59:29 +0300

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,6 +41,9 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
+                         <li class="nav-item">
+                                <a class="nav-link" href="/form">MY PROPERTY</a>
+                            </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
