845ca5a845f2edec252fea7067df1fae932a8e0a
omasette <38251408+omasette@users.noreply.github.com>
2018-07-08 14:59:43 +0300

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -25,18 +25,18 @@
                             </div>
                         </div>
 
-                        //<div class="form-group row">
-                          //  <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }} name="Name" value="{{$login->Name or ''}}</label>
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }} name="Name" value="{{$login->Name or ''}}</label>
 
-                            //<div class="col-md-6">
-                              //  <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
 
-                                //@if ($errors->has('password'))
-                                  //  <span class="invalid-feedback">
-                                    //    <strong>{{ $errors->first('password') }}</strong>
-                                    //</span>
-                               // @endif
-                            //</div>
+                              @if ($errors->has('password'))
+                                   <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            <div>
                         </div>
 
                         <div class="form-group row">
