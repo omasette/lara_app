87a2bfd7a3539f52efa203e33c7012c2ddaa2f2d
omasette <38251408+omasette@users.noreply.github.com>
2018-07-08 16:21:07 +0300

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
   </div>
 
   <div class="container">
-    <label for="email"><b>Email</b></label>
+    <label for="Email"><b>Email</b></label>
     <input type="text" placeholder="Enter Email Address" name="Email" required><br>
 
     <label for="psw"><b>Password</b></label>
