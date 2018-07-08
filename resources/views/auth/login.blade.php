04b23358b55badb772e858ac4778aa223713702c
omasette <38251408+omasette@users.noreply.github.com>
2018-07-08 15:54:57 +0300

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,10 +6,10 @@
 
   <div class="container">
     <label for="uname"><b>Username</b></label>
-    <input type="text" placeholder="Enter Username" name="uname" required>
+    <input type="text" placeholder="Enter Username" name="uname" required><br>
 
     <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required>
+    <input type="password" placeholder="Enter Password" name="psw" required><br>
 
     <button type="submit">Login</button>
     <label>
@@ -22,6 +22,7 @@
     <span class="psw">Forgot <a href="#">password?</a></span>
   </div>
   <style>
+  <center>
   form {
     border: 3px solid #f1f1f1;
 }
@@ -92,6 +93,7 @@ span.psw {
         width: 100%;
     }
 }
+</center>
   </style>
 </form> 
 </html>
