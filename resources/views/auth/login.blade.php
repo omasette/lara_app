793b7a5dc942ccc1fc88cde13411607b0428e205
omasette <38251408+omasette@users.noreply.github.com>
2018-07-08 15:33:16 +0300

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,79 +1,23 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Property</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-</head>
-<body>
-
-<div class="container">
-  <div class="topnav">
-    <a class="active" href="/">Home</a>
-    <a href="/property_table">property Table</a>
+ <form action="action_page.php">
+  <div class="imgcontainer">
+    <img src="img_avatar2.png" alt="Avatar" class="avatar">
   </div>
-  <h2>My Properties</h2>
-  <form class="form-horizontal" action="/property" method="post"  encytype = "multipart/form-data">
-{{csrf_field()}}
-    <div class="form-group">
-      <label class="control-label col-sm-2">Property Name:</label>
-      <div class="col-sm-10">
-        <input type="text" class="form-control" placeholder="Enter Property Name" name="propertyName" value="{{$property->Name or ''}}">
-      </div>
-    </div>
-    <div class="form-group">
-      <label class="control-label col-sm-2">location:</label>
-      <div class="col-sm-10">
-        <input type="text" class="form-control" placeholder="Enter property's location" name="locationName" value="{{$property->location or ''}}">
-      </div>
-    </div>
-    <div class="form-group">
-      <label class="control-label col-sm-2">Description:</label>
-      <div class="col-sm-10">
-        <input type="text" class="form-control" placeholder="Enter property'description" name="description" value="{{$property->description or ''}}" >
-      </div>
-    </div>
-    <div class="form-group">
-      <label class="control-label col-sm-2">contact:</label>
-      <div class="col-sm-10">
-        <input type="text" class="form-control" placeholder="Enter landlord's contact" name="contacts" value="{{$property->contacts or ''}}">
-      </div>
-    </div>
-    <input type="text" name="id" value="{{$property->id or ''}}" readonly>
-    <div class="form-group">
-        <div class="col-sm-offset-2 col-sm-10">
-        <button type="submit" class="btn btn-default">Submit</button>
-      </div>
-    </div>
-  </form>
-</div>
-<style>
-/* Add a black background color to the top navigation */
-.topnav {
-    background-color: #333;
-    overflow: hidden;
-}
 
-/* Style the links inside the navigation bar */
-.topnav a {
-    float: left;
-    color: #f2f2f2;
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
-    font-size: 17px;
-}
+  <div class="container">
+    <label for="uname"><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="uname" required>
 
-/* Change the color of links on hover */
-.topnav a:hover {
-    background-color: #ddd;
-    color: black;
-}
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
 
-/* Add a color to the active/current link */
-.topnav a.active {
-    background-color: #4CAF50;
-    color: white;
-}
-</style>
-</body>
-</html>
+    <button type="submit">Login</button>
+    <label>
+      <input type="checkbox" checked="checked" name="remember"> Remember me
+    </label>
+  </div>
+
+  <div class="container" style="background-color:#f1f1f1">
+    <button type="button" class="cancelbtn">Cancel</button>
+    <span class="psw">Forgot <a href="#">password?</a></span>
+  </div>
+</form> 
