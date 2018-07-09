06e011f0f6c678e1c1328b79c8280907c9a6c57b
omasette <38251408+omasette@users.noreply.github.com>
2018-07-09 17:17:39 +0000

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,7 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
+                background-color: sky blue;
                 color: #636b6f;
                 font-family: 'Raleway', sans-serif;
                 font-weight: 100;
