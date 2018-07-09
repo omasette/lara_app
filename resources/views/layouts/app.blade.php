2abd3aebb26014cadf3b619c20d6b9788b484af0
omasette <38251408+omasette@users.noreply.github.com>
2018-07-09 09:37:25 +0000

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,6 +27,9 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Nyumba Chap chap') }}
                 </a>
+                <a class="navbar-brand" href="{{ url('/property_table') }}">
+                    {{ config('app.name', 'Property') }}
+                </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
