0f7855c301dff7783c5bce9a4700271257791610
omasette <38251408+omasette@users.noreply.github.com>
2018-07-10 17:07:11 +0000

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -60,13 +60,6 @@
                                 </a>
                             </div>
                         </div>
-
-                        <p class="text-center mb-3">
-                            Or Login with
-                        </p>
-
-                        @include('partials.socials-icons')
-
                     </form>
                 </div>
             </div>
