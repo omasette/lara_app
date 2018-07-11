2bfed805b17b3f7ef740eddbe0480c80f354e1ba
omasette <38251408+omasette@users.noreply.github.com>
2018-07-11 10:23:52 +0300

    Update property_table.blade.php

--- resources/views/layouts/property_table.blade.php
+++ resources/views/layouts/property_table.blade.php
@@ -11,11 +11,18 @@
                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
                 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
                 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
+                
+                <div class="container">
+  <div class="topnav">
+    <a class="active" href="/">Home</a>
+    <a href="/form">property </a>
+  </div>
+  
                 <div class="panel-body">
                     <table >
                     <tr>
                         <th>
+                     <td>Number</td>
                     <td>Property Name </td>
                     <td>Location</td>
                     <td>Description</td>
@@ -26,6 +33,7 @@
                     </tr>
                     @foreach($property as $zawatu)
                     <tr >
+                    <td>{{$zawatu->id}}</td>
                     <td>{{$zawatu->Name}}</td>
                     <td>{{$zawatu->location}}</td>
                     <td>{{$zawatu->description}}</td>
@@ -89,6 +97,36 @@ th:hover {
     color: black;
 }
 
+</style>
+
+<style>
+/* Add a black background color to the top navigation */
+.topnav {
+    background-color: #333;
+    overflow: hidden;
+}
+
+/* Style the links inside the navigation bar */
+.topnav a {
+    float: left;
+    color: #f2f2f2;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    font-size: 17px;
+}
+
+/* Change the color of links on hover */
+.topnav a:hover {
+    background-color: #ddd;
+    color: black;
+}
+
+/* Add a color to the active/current link */
+.topnav a.active {
+    background-color: #4CAF50;
+    color: white;
+}
 </style>
 
 </body>
