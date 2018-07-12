c80a40019cbe5ea391eeef1254e54eebdd08211d
omasette <38251408+omasette@users.noreply.github.com>
2018-07-12 11:30:46 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -40,3 +40,15 @@ Route::get('/property_table', 'NyumbachapchapController@show');
 Route::get('/form', 'NyumbachapchapController@index');
 Route::get('/propertyupdate/{id}', 'NyumbachapchapController@update');
 Route::get('/propertydestroy/{id}', 'NyumbachapchapController@destroy');
+
+
+/*photo portfolio routes*/
+Route::get('/', array('as' => 'index','uses' => 'AlbumsController@getList'));
+Route::get('/createalbum', array('as' => 'create_album_form','uses' => 'AlbumsController@getForm'));
+Route::post('/createalbum', array('as' => 'create_album','uses' => 'AlbumsController@postCreate'));
+Route::get('/deletealbum/{id}', array('as' => 'delete_album','uses' => 'AlbumsController@getDelete'));
+Route::get('/album/{id}', array('as' => 'show_album','uses' => 'AlbumsController@getAlbum'));
+Route::get('/addimage/{id}', array('as' => 'add_image','uses' => 'ImageController@getForm'));
+Route::post('/addimage', array('as' => 'add_image_to_album','uses' => 'ImageController@postAdd'));
+Route::get('/deleteimage/{id}', array('as' => 'delete_image','uses' => 'ImageController@getDelete'));
+Route::post('/moveimage', array('as' => 'move_image', 'uses' => 'ImageController@postMove'));
